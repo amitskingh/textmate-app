5ccead1a5ceb5e87561cbd9392833f18484609ca
Amit Singh <amitsskingh@gmail.com>
2024-12-04 16:33:14 +0530

    update

--- src/component/ContextMenu.jsx
+++ src/component/ContextMenu.jsx
@@ -6,15 +6,15 @@ export function ContextMenu() {
     <>
       <div className="z-50 absolute shadow-slate-500 rounded-sm bg-white shadow top-0 right-6 h-32 w-32">
         <ul className="">
-          <li className="text-slate-700 hover:bg-black hover:text-white cursor-pointer mx-auto px-2 py-1">
+          <li className="text-slate-700 hover:bg-black hover:text-white cursor-pointer mx-auto p-2">
             <FolderOpenIcon className="size-4 inline-block mr-2" />
             Open
           </li>
-          <li className="text-slate-700 hover:bg-black hover:text-white cursor-pointer mx-auto px-2 py-1">
+          <li className="text-slate-700 hover:bg-black hover:text-white cursor-pointer mx-auto p-2">
             <CgRename className="size-4 inline-block mr-2" />
             Rename
           </li>
-          <li className="text-slate-700 hover:bg-black hover:text-white cursor-pointer mx-auto px-2 py-1">
+          <li className="text-slate-700 hover:bg-black hover:text-white cursor-pointer mx-auto p-2">
             <TrashIcon className="size-4 inline-block mr-2" />
             Delete
           </li>
--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -26,11 +26,7 @@ export default function Books() {
         onMouseLeave={() => handleBottomBorder(false)} // Hover ends
         className="md:grid md:grid-rows-4 flex flex-col max-w-[768px] mx-auto"
       >
-        <div
-          onTouchStart={() => handleBottomBorder(true)} // For touch devices
-          onTouchEnd={() => handleBottomBorder(false)}
-          className="relative my-3 mx-2 px-2 flex items-center pb-1"
-        >
+        <div className="relative my-3 mx-2 px-2 flex items-center pb-1">
           <BsJournalRichtext className="size-12 mr-2" />
           <div className="w-full">
             <div className="relative flex justify-between">
@@ -51,7 +47,6 @@ export default function Books() {
             <div className="absolute inset-x-0 bottom-0 bg-gradient-to-r from-blue-500 via-purple-500 to-pink-500 mx-2 h-[3px]" />
           )}
           {isOpenMenu && <ContextMenu />}{" "}
-          {/* Conditionally render ContextMenu based on isOpenMenu */}
         </div>
       </div>
     </>
