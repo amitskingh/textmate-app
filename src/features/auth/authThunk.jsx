c28f833da27d5272727408d6b597653dc1d9bd5e
Amit Singh <amitsskingh@gmail.com>
2024-12-31 21:23:55 +0530

    update:

--- src/features/auth/authThunk.jsx
+++ src/features/auth/authThunk.jsx
@@ -1,17 +1,15 @@
 import { createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
+import axiosInstance from "../../api/axiosConfig"
 
 export const loginThunk = createAsyncThunk(
   "auth/login",
   async ({ email, password }, { rejectWithValue }) => {
     try {
-      const response = await axios.post(
-        "http://localhost:3000/api/v1/auth/login",
-        {
-          email,
-          password,
-        }
-      )
+      const response = await axiosInstance.post("/auth/login", {
+        email,
+        password,
+      })
       return response.data
     } catch (error) {
       const errorMessage =
@@ -26,14 +24,11 @@ export const registerThunk = createAsyncThunk(
   "auth/register",
   async ({ name, email, password }, { rejectWithValue }) => {
     try {
-      const response = await axios.post(
-        "http://localhost:3000/api/v1/auth/register",
-        {
-          name,
-          email,
-          password,
-        }
-      )
+      const response = await axiosInstance.post("/auth/register", {
+        name,
+        email,
+        password,
+      })
 
       return response.data
     } catch (error) {
